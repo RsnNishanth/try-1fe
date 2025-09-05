cb899c7f59916ad23b9578e5b3a30e061b073298
NISHANTH_S <nishanthsivaraj@gmail.com>
2025-09-05 11:32:54 +0530

    ft

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -108,13 +108,18 @@ const buyCart = async () => {
           </Card>
         ))}
       </div>
-{!isLoading && !(Array.isArray(cart) || cart.length === 0) && (
-        <div className="center-state">
-         <Button variant="success" className="buy-cart-btn" onClick={buyCart}>
-  Buy Cart
-</Button>
-        </div>
-      )}
+{cart.length > 0 && (
+  <div className="center-state">
+    <Button 
+      variant="success" 
+      className="buy-cart-btn" 
+      onClick={buyCart}
+    >
+      Buy Cart
+    </Button>
+  </div>
+)}
+
       
 
       {error && <div className="error-message">Error: {error}</div>}
